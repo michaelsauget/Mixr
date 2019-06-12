92da0b8b336c8b9924389c4f6e8f11a54a04998b
Michael Sauget <michaelsauget@gmail.com>
2019-06-12 04:01:01 -0400

    Modification du app.module pour la nouvelle page

--- appmobile/src/app/app-routing.module.ts
+++ appmobile/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'cocktail-presentation', loadChildren: './cocktail-presentation/cocktail-presentation.module#CocktailPresentationPageModule' }
 ];
 @NgModule({
   imports: [
