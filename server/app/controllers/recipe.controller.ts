5a032b6dda75dceb71777a7ef26c5fdc4b4120f5
Michael Sauget <michaelsauget@gmail.com>
2019-06-12 17:32:45 -0400

    Enlève le code mort

--- server/app/controllers/recipe.controller.ts
+++ server/app/controllers/recipe.controller.ts
@@ -14,18 +14,9 @@ export class RecipeController {
     public get router(): Router {
         const router: Router = Router();
 
-        router.get("/:cocktailno", async (req: Request, res: Response, next: NextFunction) => {
+        router.get("/cocktailNo/:cocktailno", async (req: Request, res: Response, next: NextFunction) => {
             res.send(await this.recipeService.getCocktailRecipe(req.params.cocktailno));
-            // res.send("hola du recipe controller !");
         });
-
-        // router.get("/searchById/:id", async (req: Request, res: Response, next: NextFunction) => {
-        //     res.send(await this.cocktailService.getById(req.params.id));
-        // });
-
-        // router.get("/searchByName/:entry", async (req: Request, res: Response, next: NextFunction) => {
-        //     res.send(await this.cocktailService.searchByName(req.params.entry));
-        // });
 
         return router;
     }
