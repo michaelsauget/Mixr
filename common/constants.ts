b7ff5025b7714e21ad4dd2d240d49c904b5bbfa4
Michael Sauget <michaelsauget@gmail.com>
2019-06-13 17:35:45 -0400

    Add a few constants concerning the database informations.

--- common/constants.ts
+++ common/constants.ts
@@ -1,14 +1,21 @@
 export class Constants {
 
     // public static readonly BASE_URL:         string = "http://localhost";
-    public static readonly SERVER_URL:         string = "http://192.168.2.116";
+   
+    // public static readonly SERVER_URL:         string = "http://192.168.2.116";
+   
+   // public static readonly SERVER_URL:         string = "http://192.168.2.252";
+    
+
+    public static readonly SERVER_URL:         string = "http://70.51.45.213";
     public static readonly SERVER_PORT:        string = "3000";
     
-    public static readonly DATABASE_SERVER_URL:     string = "144.172.153.80";
-    public static readonly DATABASE_SERVER_PORT:    string = "50000";
-    public static readonly DATABASE_PASSWORD:       string = "Action500";
+    public static readonly DATABASE_SERVER_URL:     string = "192.168.2.252";
+    public static readonly DATABASE_SERVER_PORT:    string = "5432";
+    public static readonly DATABASE_PASSWORD:       string = "password123";
     public static readonly DATABASE_SCHEMA:         string = "mixerr";
-    
+    public static readonly DATABASE_NAME:           string = "mixr";
+    public static readonly DATABASE_USER:           string = "dbadmin";
 
 
     public static readonly API_COCKTAIL:            string = "/api/cocktail/";
