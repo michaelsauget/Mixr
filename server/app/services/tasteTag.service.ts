6b8a580574d9efd1e2ca8c5e7cee626363ac3028
Michael Sauget <michaelsauget@gmail.com>
2019-06-18 04:07:41 -0400

    Classe tasteTag.service.ts fonctionnelle. Enlève le code mort.

--- server/app/services/tasteTag.service.ts
+++ server/app/services/tasteTag.service.ts
@@ -1,75 +1,74 @@
 
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import "reflect-metadata";
-// import { iCocktailIngredient, iIngredient, iIngredientType, iRecipe, iRecipeQuery } from "../../../common/interfaces";
-import { DatabaseConnectionService } from "./databaseConnection.service";
-// import { iTasteTag, iTasteTagQuery } from "../../../common/interfaces";
+import { iTasteTag, iTasteTagQuery } from "../../../common/interfaces";
 
-// tslint:disable-next-line: variable-name no-any
-// const Sequelize: any = require("sequelize");
+import Types from "../types";
+import { DatabaseConnectionService } from "./databaseConnection.service";
+import { InterfaceBuilder } from "./interfaceBuilder";
+
+// tslint:disable-next-line:variable-name typedef
+const Sequelize = require("sequelize");
 
 @injectable()
-export class TasteTagService extends DatabaseConnectionService {
+export class TasteTagService {
 
-    public constructor() {
-        super();
+    private tasteTag: any;
+
+    public constructor(
+        @inject(Types.InterfaceBuilder) private builder: InterfaceBuilder,
+        @inject(Types.DatabaseConnectionService) private databaseService: DatabaseConnectionService,
+    ) {
+        this.defineTasteTag();
     }
-    public async getAllTags(): Promise<void> { /* */ }
 
-    // public async getAllTags(): Promise<iTasteTagQuery> {
-        // return this.connection.query(
-            // tslint:disable-next-line:max-line-length
-            // "SELECT cockIng.quantity AS quantity, ing.ingredientno, ing.ingredientname, prep.preposition, ing.ingredienttypeno, ingT.ingredienttype FROM Mixerr.cocktailingredients AS cockIng INNER JOIN Mixerr.ingredient AS ing ON cockIng.ingredientno   = ing.ingredientno INNER JOIN Mixerr.ingredientprepositions AS ingPrep ON ing.ingredientno = ingPrep.ingredientno INNER JOIN Mixerr.preposition AS prep ON ingPrep.prepositionno = prep.prepositionno INNER JOIN Mixerr.cocktail AS cock ON cockIng.cocktailno = cock.cocktailno INNER JOIN Mixerr.ingredienttype AS ingT ON ing.ingredienttypeno = ingT.typeno WHERE cock.cocktailno = " + "\'" + cocktailNo + "\';",
-            // { type: Sequelize.QueryTypes.SELECT})
-        // tslint:disable-next-line:no-any
-        // .then((results: any) => {
-            // return { hasBeenFound: true, foundRecipe: this.tasteTagBuilder(results, cocktailNo) } as iTasteTagQuery;
-        // })
-        // .catch((err: Error) => {
-            // return { hasBeenFound: false } as iTasteTagQuery;
-        // });
-    // }
+    public async getAllTags(): Promise<iTasteTagQuery> {
+        return this.tasteTag.findAll({raw: true})
+        .then((query: any) => {
+            return { hasBeenFound: true, tags: this.getMappedTags(query) };
+        })
+        .catch((err: Error) => {
+            return { hasBeenFound: false };
+        });
+    }
 
-    // public async getCocktailRecipe(cocktailNo: string): Promise<iRecipeQuery> {
-    //     return this.connection.query(
-            // tslint:disable-next-line:max-line-length
-    //         "SELECT cockIng.quantity AS quantity, ing.ingredientno, ing.ingredientname, prep.preposition, ing.ingredienttypeno, ingT.ingredienttype FROM Mixerr.cocktailingredients AS cockIng INNER JOIN Mixerr.ingredient AS ing ON cockIng.ingredientno   = ing.ingredientno INNER JOIN Mixerr.ingredientprepositions AS ingPrep ON ing.ingredientno = ingPrep.ingredientno INNER JOIN Mixerr.preposition AS prep ON ingPrep.prepositionno = prep.prepositionno INNER JOIN Mixerr.cocktail AS cock ON cockIng.cocktailno = cock.cocktailno INNER JOIN Mixerr.ingredienttype AS ingT ON ing.ingredienttypeno = ingT.typeno WHERE cock.cocktailno = " + "\'" + cocktailNo + "\';",
-    //         { type: Sequelize.QueryTypes.SELECT})
-    //     // tslint:disable-next-line:no-any
-    //     .then((results: any) => {
-    //         return { hasBeenFound: true, foundRecipe: this.recipeBuilder(results, cocktailNo) } as iRecipeQuery;
-    //     })
-    //     .catch((err: Error) => {
-    //         return { hasBeenFound: false } as iRecipeQuery;
-    //     });
-    // }
+    private getMappedTags(tagsToMap: any): Map<number, iTasteTag[]> {
+        const tagMap: Map<number, iTasteTag[]> = new Map<number, iTasteTag[]>();
 
-    // private tasteTagBuilder(foundTags: any): iRecipe {
-    //     const cocktailIngredients: iTasteTag[] = [];
+        tagsToMap.forEach((tag: any) => {
+            const cocktail: iTasteTag[] | undefined = tagMap.get(Number(tag.cocktailno));
 
-    //     foundIngredients.forEach((ing: any) => {
-    //         cocktailIngredients.push();
-    //     });
+            if (cocktail) {
+                cocktail.push(this.builder.buildTasteTag(tag));
+            } else {
+                tagMap.set(Number(tag.cocktailno), [this.builder.buildTasteTag(tag)]);
+            }
+        });
 
-    //     return {
-    //         cocktailno:             cocktailNo,
-    //         cocktailIngredients:    cocktailIngredients,
-    //     } as iRecipe;
-    // }
+        return tagMap;
+    }
 
-    // public cocktailIngredientBuilder(ingredient: any): iCocktailIngredient {
-    //     return {
-    //         quantity:       ingredient.quantity,
-    //         ingredient: {
-    //             ingredientNo:   ingredient.ingredientno,
-    //             ingredientName: ingredient.ingredientname,
-    //             preposition:    ingredient.preposition,
-    //             ingredientType: {
-    //                 typeno: ingredient.typeno,
-    //                 type:   ingredient.ingredienttype,
-    //             } as iIngredientType,
-    //         } as iIngredient,
-    //     } as iCocktailIngredient;
-    // }
-
+    private defineTasteTag(): void {
+        this.tasteTag  = this.databaseService.connection.define(
+            "v_cocktailtags",
+            {   // attributes
+                tagno: {
+                    type:       Sequelize.STRING,
+                    allowNull:  false,
+                    primaryKey: true,
+                },
+                taste: {
+                    type:       Sequelize.STRING,
+                    allowNull:  false,
+                },
+                cocktailno: {
+                    type:       Sequelize.STRING,
+                },
+            },
+            {
+                schema:             "",
+                timestamps:         false,
+                freezeTableName:    true,
+            });
+    }
 }
